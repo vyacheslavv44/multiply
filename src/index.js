d33c4f938ca047a869f27c6d9480d88e4ef3dd58
vyacheslavv44 <follow.trend7777@gmail.com>
2019-10-12 23:22:51 +0400

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function multiply(first, second) {
   // your solution
+  //var first = '5';
+  //var first1 = first.value;
+ // var second = '10';
+  //var second1 = second.value;
+  //var a = Number(first);
+  //var b = Number(second);
+  //var a = parseInt(first);
+  //var b = parseInt(second);
+  var a = BigInt(first);
+  var b = BigInt(second);
+  var c = a * b;
+  var d = c.toString();
+  return d;
 }
+
+
